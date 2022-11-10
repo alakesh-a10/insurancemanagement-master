6308c4a60c5c4a31616b8a04e4dee0d7124b791f
Alakesh Sarma <icvc.a10@gmail.com>
2022-11-10 12:29:39 +0530

    appsconfig

--- insurancemanagement/settings.py
+++ insurancemanagement/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
-    'insurance.app.InsuranceConfig',
-    'customer.app.CustomerConfig',
+    'insurance.apps.InsuranceConfig',
+    'customer.apps.CustomerConfig',
 ]
 
 MIDDLEWARE = [
